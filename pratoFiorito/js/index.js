e7e67149ade8603f79ca1536a6acbbf35b49ab8f
Alessandro-Francia <73991843+Alessandro-Francia@users.noreply.github.com>
2023-03-30 19:15:24 +0200

    Add files via upload

--- pratoFiorito/js/index.js
+++ pratoFiorito/js/index.js
@@ -8,20 +8,9 @@ const memoryTable = [
 	[0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0]
-  ]
-  /* 
-  const memoryTable = [
-	[new NumberCell(0, 0), new Bomb(1, 0) , new Bomb(2, 0), 0, 0, 0, 0, 0, 0],
-	[new Bomb(0, 1), new NumberCell(1, 1) , new Bomb(2, 1), 0, 0, 0, 0, 0, 0],
-	[new Bomb(0, 2), new Bomb(1, 2), new Bomb(2, 2), 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0]
-  ] */
-  class Entity {
+	]
+
+class Entity {
 	constructor(x, y, hidden, flagged){
 	  this.x = x
 	  this.y = y
@@ -29,9 +18,9 @@ const memoryTable = [
 	  this.flagged = flagged
 	  this.click = () => {}
 	}
-  }
+}
   
-  class Bomb extends Entity {
+class Bomb extends Entity {
 	constructor(x, y, hidden = true, flagged = false){
 	  super(x, y, hidden, flagged)
 	  this.click = () => {
@@ -44,9 +33,9 @@ const memoryTable = [
 		  return `<div id="cell_${this.x}_${this.y}" class="Bomb" onclick="cellClicked(${this.x}, ${this.y})"> </div>`
 	  }
 	}
-  }
+}
   
-  class NumberCell extends Entity {
+class NumberCell extends Entity {
 	constructor(x, y,  hidden = true, flagged = false, number = 0){
 	  super(x, y, hidden, flagged)
 	  this.number = number
@@ -62,56 +51,56 @@ const memoryTable = [
 	numberCheck = () => {
 	  let counter = 0
 	  try {
-		if (memoryTable[this.y-1][this.x-1].constructor.name === "Bomb") {
+		if (memoryTable[this.x-1][this.y-1].constructor.name === "Bomb") {
 		  counter++
 		}
 	  } catch (error) {
 		console.log(error)
 	  }
 	  try {
-		if (memoryTable[this.y-1][this.x].constructor.name === "Bomb") {
+		if (memoryTable[this.x][this.y-1].constructor.name === "Bomb") {
 		  counter++
 		}
 	  } catch (error) {
 		console.log(error)
 	  }
 	  try {
-		if (memoryTable[this.y-1][this.x+1].constructor.name === "Bomb") {
+		if (memoryTable[this.x+1][this.y-1].constructor.name === "Bomb") {
 		  counter++
 		}
 	  } catch (error) {
 		console.log(error)
 	  }
 	  try {
-		if (memoryTable[this.y][this.x-1].constructor.name === "Bomb") {
+		if (memoryTable[this.x-1][this.y].constructor.name === "Bomb") {
 		  counter++
 		}
 	  } catch (error) {
 		console.log(error)
 	  }
 	  try {
-		if (memoryTable[this.y][this.x+1].constructor.name === "Bomb") {
+		if (memoryTable[this.x+1][this.y].constructor.name === "Bomb") {
 		  counter++
 		}
 	  } catch (error) {
 		console.log(error)
 	  }
 	  try {
-		if (memoryTable[this.y+1][this.x-1].constructor.name === "Bomb") {
+		if (memoryTable[this.x-1][this.y+1].constructor.name === "Bomb") {
 		  counter++
 		}
 	  } catch (error) {
 		console.log(error)
 	  }
 	  try {
-		if (memoryTable[this.y+1][this.x].constructor.name === "Bomb") {
+		if (memoryTable[this.x][this.y+1].constructor.name === "Bomb") {
 		  counter++
 		}
 	  } catch (error) {
 		console.log(error)
 	  }
 	  try {
-		if (memoryTable[this.y+1][this.x+1].constructor.name === "Bomb") {
+		if (memoryTable[this.x+1][this.y+1].constructor.name === "Bomb") {
 		  counter++
 		}
 	  } catch (error) {
@@ -120,7 +109,7 @@ const memoryTable = [
 	  this.number = counter
 	  return counter
 	}
-  }
+}
   /*
   class VoidCell extends Entity {
 	constructor(x, y, hidden = true, flagged = false){
@@ -196,16 +185,16 @@ const memoryTable = [
   }
   */
   
-  const tableGenerator = () => {
+const tableGenerator = () => {
   
-	  const tempMemoryTable = []
-  
-	  memoryTable.forEach( (e, i) => {
-	  e.forEach((elem, ind) => {
-		memoryTable[ind][i] = bombGenerator(ind, i)
-		console.log(elem)			
-		  })
+  const tempMemoryTable = []
+ 
+  memoryTable.forEach( (e, y) => {
+  e.forEach((elem, x) => {
+	memoryTable[x][y] = bombGenerator(x, y)
+	console.log(elem)			
 	  })
+  })
   
   /* 	memoryTable.forEach( (e, i) => {
 		  e.forEach((elem, ind) => {
@@ -213,40 +202,59 @@ const memoryTable = [
 		  });		
 	  }); */
   
-	  const container = document.getElementById("container")
-	  container.innerHTML = ""
+	const container = document.getElementById("container")
+		container.innerHTML = ""
 	  
 	  memoryTable.forEach((e,i) => {
-		  container.innerHTML += `<div id="row_${i}" class="row"> </div>`
-		  e.forEach((elem, ind) => {
-			  let row = document.getElementById(`row_${i}`)
-		row.innerHTML += elem.render()
-  
-		  })
-	  })
+		container.innerHTML += `<div id="column_${i}" class="column"> </div>`
+		e.forEach((elem, ind) => {
+			let column = document.getElementById(`column_${i}`)
+			column.innerHTML += elem.render()
+			})
+		})
 	console.log(memoryTable)
-	
   }
   
   const bombGenerator = (x, y) => {
 	  let bombChance = Math.floor(Math.random() * 10)
-  
 	  if (bombChance <= 2) {
-		  return new Bomb(x,y)
+			return new Bomb(x, y)
 	  } else {
-		  return new NumberCell(x,y)
+		  return new NumberCell(x, y)
 	  }
   }
   
-  const bombDrawControl = (x, y) => {
+ /*  const bombDrawControl = (x, y) => {
 	  console.log(memoryTable[y], memoryTable[y][x])
 	  if (memoryTable[y][x] == -1) {
 		  return true
 	  } else {
 		  return false
 	  }
-  }
+  } */
   
-  const cellClicked = (x, y) => {
-	  console.log(`hai premuto la cesella n.${x} - ${y}`)
-  }
+const cellClicked = (x, y) => {
+  console.log (`hai premuto la cesella n.${x} - ${y}`)
+	let cell = memoryTable[x][y]
+	console.log (cell)
+	console.log (cell.numberCheck())
+	
+
+}
+
+const prova = () => {
+	console.log (memoryTable)
+	/*const swap = (x, y) => {
+		return x, y 
+	}
+	const tempMemoryTable = memoryTable.map(swap)
+
+	for {}
+
+	/*memoryTable.forEach ((e, i) => {
+		memoryTable.forEach ((elem, ind) => {
+			tempMemoryTable [ind][i] = memoryTable [i][ind]
+		})
+	})
+	console.log(tempMemoryTable)*/
+}
